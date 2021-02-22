2b68cc29e7b884ca210873ec50d9d0e7c34be6a4
GautierMchs <gautier19160@gmail.com>
2021-02-22 10:47:17 +0100

    security nettoyé

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -19,7 +19,7 @@ class SecurityController extends AbstractController
     /**
      * @Route("/cconnexion", name="secondLife_connexion")
      */
-    public function logn(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         
         // if ($this->getUser()) {
@@ -43,7 +43,7 @@ class SecurityController extends AbstractController
     /**
      * @Route("/deconnexion", name="secondLife_deconnexion")
      */
-    public function logut()
+    public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
@@ -91,32 +91,5 @@ class SecurityController extends AbstractController
      */
     public function connexion(){
         return $this->render('security/login.html.twig');
-    }
-
-    
-
-    /**
-     * @Route("/login", name="app_login")
-     */
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
-
-    /**
-     * @Route("/logout", name="app_logout")
-     */
-    public function logout()
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
